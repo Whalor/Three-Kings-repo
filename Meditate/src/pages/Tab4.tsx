e8cac5fd79bb47e039f4e9b307485766e28e72da
PhoenixFire432 <60374988+PhoenixFire432@users.noreply.github.com>
2020-05-02 21:32:43 -0600

    Update Tab4.tsx

--- Meditate/src/pages/Tab4.tsx
+++ Meditate/src/pages/Tab4.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab3.css';
+import { IonContent, IonButton, IonInput, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonItem } from '@ionic/react';
+import './Tab4.css';
 
-const Tab4: React.FC = () => {
+interface ContainerProps {
+  name: string;
+}
+
+const Tab4: React.FC<ContainerProps> = ({name}) => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Profile</IonTitle>
+    
+    <IonPage >
+      <IonContent class="background-image5">
+      <IonHeader class="icon"> <p></p>
+        <IonToolbar><p></p>
+          <IonTitle><p>Welcome, to Meditate With Me :)</p> </IonTitle>
         </IonToolbar>
       </IonHeader>
+        <IonCard >
+            <IonCardHeader >
+              <h3>Tap the Yellow Button to Open App</h3>
+            </IonCardHeader>
+            
+          </IonCard>
+          
+          <div className="container4">
+          <strong>{name}</strong>
+          <button className="button4" type="button"><a target="" rel="noopener noreferrer" href="http://localhost:8100/tab1">__</a></button>
+          </div> 
+      </IonContent>
+      
     </IonPage>
   );
 };
