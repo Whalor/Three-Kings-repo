c6725d5a2de4a2ca2ae555730a64cd71a261b16c
PhoenixFire432 <60374988+PhoenixFire432@users.noreply.github.com>
2020-05-02 21:32:39 -0600

    Update Tab3.tsx

--- Meditate/src/pages/Tab3.tsx
+++ Meditate/src/pages/Tab3.tsx
@@ -5,11 +5,13 @@ import './Tab3.css';
 const Tab3: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonContent class="background-image3">
+      <IonHeader class="icon">
         <IonToolbar>
           <IonTitle>Settings</IonTitle>
         </IonToolbar>
       </IonHeader>
+      </IonContent>
     </IonPage>
   );
 };
